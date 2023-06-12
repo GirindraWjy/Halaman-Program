bc52bc4be846e20105ea3c90603a1f77dbcf0481
GirindraWjy <110369335+GirindraWjy@users.noreply.github.com>
2023-06-12 15:18:18 +0700

    Add files via upload

--- program/script.js
+++ program/script.js
@@ -1,10 +1,39 @@
-firstImg = carousel.querySelectorAll("img")[0]
+const carousel = document.querySelector('.carousel');
+firstImg = carousel.querySelectorAll("img")[0];
 arrowIcons = document.querySelectorAll(".wrapper i");
 
+let isDragStart = false, prevPageX, prevScrollLeft;
 let firstImgWidth = firstImg.clientWidth + 14;
 
-arrowIcons.array.forEach(icon => {
+arrowIcons.forEach(icon => {
     icon.addEventListener("click", () => {
-        console.log(icon);
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
     });
 });
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging")
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+}
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mousedown", dragStop);
+carousel.addEventListener("touchend", dragStop);
