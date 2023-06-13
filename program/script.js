c3cdda0a27ae76bf8a3debd6895fadddd357e3f4
GirindraWjy <110369335+GirindraWjy@users.noreply.github.com>
2023-06-13 14:35:00 +0700

    update 13-06-2023

--- program/script.js
+++ program/script.js
@@ -2,7 +2,7 @@ const carousel = document.querySelector('.carousel');
 firstImg = carousel.querySelectorAll("img")[0];
 arrowIcons = document.querySelectorAll(".wrapper i");
 
-let isDragStart = false, prevPageX, prevScrollLeft;
+let isDragStart = false, prevPageX, prevScrollLeft, positionDiff;
 let firstImgWidth = firstImg.clientWidth + 14;
 
 arrowIcons.forEach(icon => {
@@ -12,7 +12,7 @@ arrowIcons.forEach(icon => {
 });
 const dragStart = (e) => {
     isDragStart = true;
-    prevPageX = e.pageX;
+    prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 
@@ -20,7 +20,7 @@ const dragging = (e) => {
     if(!isDragStart) return;
     e.preventDefault();
     carousel.classList.add("dragging")
-    let positionDiff = e.pageX - prevPageX;
+    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
 }
 
@@ -35,5 +35,17 @@ carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("touchmove", dragging);
 
 carousel.addEventListener("mouseup", dragStop);
-carousel.addEventListener("mousedown", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
 carousel.addEventListener("touchend", dragStop);
+
+function validateForm() {
+  var name = document.forms["form-registration"]["nama_lengkap"].value;
+  var instansi = document.forms["form-registration"]["nama_panggilan"].value;
+  var email = document.forms["form-registration"]["email"].value;
+  var phoneNumber = document.forms["form-registration"]["nomor_hp"].value;
+
+  if (name === "" || instansi === "" || email === "" || phoneNumber === "") {
+    alert("Mohon lengkapi semua field sebelum mengirimkan form.");
+    return false;
+  }
+}
